d595dc80ae4bf6cac9868818e5ebc91e3c8b9e6f
Yeseul Jung <shley0527@naver.com>
2022-02-21 23:13:55 +0900

    login logout page

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 import os, environ
 
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -139,3 +141,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
