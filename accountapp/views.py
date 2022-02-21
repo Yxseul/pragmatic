6e1e66ad29775496e40b834c9276f1748058c55a
Yeseul Jung <shley0527@naver.com>
2022-02-21 15:37:11 +0900

    django course 9 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
