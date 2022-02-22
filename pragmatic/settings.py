4840cfcb98aa80387f7e41f9124fa23d8f2ea53f
Yeseul Jung <shley0527@naver.com>
2022-02-22 14:03:03 +0900

    profile view message, update_profile

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -146,3 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
