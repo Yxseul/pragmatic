4e44d035c2369963bc9c03f662c30b957b21cb41
Yeseul Jung <shley0527@naver.com>
2022-02-22 21:43:11 +0900

    commentapp, mixin

---
+++ commentapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from commentapp.views import CommentCreateView
+
+app_name = 'commentapp'
+
+urlpatterns = [
+    path('create/', CommentCreateView.as_view(), name='create'),
+
+]
