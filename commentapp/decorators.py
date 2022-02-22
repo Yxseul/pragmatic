dd7ec0c4e27b58d214a541faacbeb40df6b75072
Yeseul Jung <shley0527@naver.com>
2022-02-22 23:55:01 +0900

    delete error 还存在, 以后要修改â

---
+++ commentapp/decorators.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponseForbidden
+
+from articleapp.models import Article
+from commentapp.models import Comment
+
+
+def comment_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+        comment = Comment.objects.get(pk=kwargs['pk'])
+        if not comment.writer == request.user:
+            return HttpResponseForbidden()
+        return func(request, *args, **kwargs)
+    return decorated
--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from commentapp.views import CommentCreateView
+from commentapp.views import CommentCreateView, CommentDeleteView
 
 app_name = 'commentapp'
 
 urlpatterns = [
     path('create/', CommentCreateView.as_view(), name='create'),
+    path('delete/', CommentDeleteView.as_view(), name='delete'),
 
 ]
